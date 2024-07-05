b476d8497c9a1bdad744e88165fdb7d546a7d9a6
Jaymil Dabhi <jaymildabhi49@gmail.com>
2024-07-05 12:25:30 +0530

    init

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,4 +1,4 @@
-export const baseUrl =  'https://home.openweathermap.org/api_keys'
-export const api_key = '8c0ea53b25d27b3d1a21b1ce83b62d71'
+export const baseUrl =  'https://api.openweathermap.org/data/2.5/weather'
+export const api_key = 'b7aa167cac50d18016aa53de62bb2126'
 
 const searchUrl = `${baseUrl}?key=8c0ea53b25d27b3d1a21b1ce83b62d71=lon`
